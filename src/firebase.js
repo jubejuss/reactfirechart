7adf398ae7383190429933ccdaa1ce24e703dc2f
Juho Kalberg <juss@vilenkell.eu>
2022-01-03 22:43:31 +0200

    ENv file

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyBKlETo9XE0yNsh4slscy-3BGUmEN59V7Q',
-  authDomain: 'koroonasurmad-a2117.firebaseapp.com',
-  databaseURL:
-    'https://koroonasurmad-a2117-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'koroonasurmad-a2117',
-  storageBucket: 'koroonasurmad-a2117.appspot.com',
-  messagingSenderId: '110475100868',
-  appId: '1:110475100868:web:f191e17ba63146a3843f91',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const db = firebaseApp.firestore();
